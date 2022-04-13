91b06e5c6ddae824b3ed551cb8ffab444c432d2f
Cinny <cinnymaria@gmail.com>
2022-04-13 23:08:07 +0200

    constructor setup

---
+++ weather.js
@@ -0,0 +1,20 @@
+class weather {
+    constructor(city, state) {
+            this.apiKey = '7e1ced1c8b8048749ced1c8b801874ff'
+            this.city = city;
+            this.state = state;
+        }
+        //Fetch weather from API
+    async getWeather() {
+            const response = await fetch(`https://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+
+            const responseData = await response.json();
+
+            return responseData.current_observation;
+        }
+        //change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
